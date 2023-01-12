e2b57d13508f9143594b2f01f60e54630b32baeb
inesber <55463738+inesber@users.noreply.github.com>
2023-01-12 00:51:33 +0000

    added files and created folder

--- editor.py
+++ editor.py
@@ -1,1 +1,12 @@
-print("hello")
+import glob
+import os
+import shutil
+
+inputs = glob.glob("inputs/*.jpg")
+
+os.makedirs("results", exist_ok=True)
+
+for filepath in inputs:
+    result_path = filepath.replace("inputs", "results")
+    shutil.copyfile(filepath, result_path)
+    print(filepath)
